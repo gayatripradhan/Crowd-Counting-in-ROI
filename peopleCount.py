0eb5d6d5b871c8fe3df4fdc3de95b382cfcc8903
gayatripradhan <38799708+gayatripradhan@users.noreply.github.com>
2020-07-25 00:09:52 +1000

    Add files via upload

--- peopleCount.py
+++ peopleCount.py
@@ -80,9 +80,7 @@ roi_x,roi_y,roi_w,roi_h = roi
 while True:
     # read the next frame from the file
     (grabbed, frame) = vs.read()
-    
-    # if the frame was not grabbed, then we have reached the end
-    # of the stream
+
     if not grabbed:
         break
 
@@ -92,18 +90,14 @@ while True:
     if W is None or H is None:
         (H, W) = frame.shape[:2]
         
-    # construct a blob from the input frame and then perform a forward
-    # pass of the YOLO object detector, giving us our bounding boxes
-    # and associated probabilities
+
     blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
 		swapRB=True, crop=False)
     net.setInput(blob)
     start = time.time()
     layerOutputs = net.forward(ln)
     end = time.time()
-    
-    # initialize our lists of detected bounding boxes, confidences,
-    # and class IDs, respectively
+
     boxes = []
     confidences = []
     classIDs = []
@@ -112,37 +106,24 @@ while True:
     for output in layerOutputs:
         # loop over each of the detections
         for detection in output:
-            # extract the class ID and confidence (i.e., probability)
-            # of the current object detection
+
             scores = detection[5:]
             classID = np.argmax(scores)
             confidence = scores[classID]
-            
-            # filter out weak predictions by ensuring the detected
-            # probability is greater than the minimum probability
+
             if confidence > args["confidence"]:
-                # scale the bounding box coordinates back relative to
-                # the size of the image, keeping in mind that YOLO
-                # actually returns the center (x, y)-coordinates of
-                # the bounding box followed by the boxes' width and
-                # height
+
                 box = detection[0:4] * np.array([W, H, W, H])
                 (centerX, centerY, width, height) = box.astype("int")
-                
-                # use the center (x, y)-coordinates to derive the top
-                # and and left corner of the bounding box
+
                 x = int(centerX - (width / 2))
                 y = int(centerY - (height / 2))
-                
-                # update our list of bounding box coordinates,
-                # confidences, and class IDs
+
                 centers.append([centerX,centerY])
                 boxes.append([x, y, int(width), int(height)])
                 confidences.append(float(confidence))
                 classIDs.append(classID)
                 
-    # apply non-maxima suppression to suppress weak, overlapping
-    # bounding boxes
     idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
 		args["threshold"])
     
@@ -170,27 +151,27 @@ while True:
     
     cv2.putText(frame, "Total no. of " + str(object_name) + " in ROI: " + str(people_count), (40, 40),cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
             
-    cv2.imshow("image",frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-            break  
+#    cv2.imshow("image",frame)
+#    if cv2.waitKey(1) & 0xFF == ord('q'):
+#            break  
     
-#    if writer is None:
-#        # initialize our video writer
-#        fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-#        writer = cv2.VideoWriter(args["output"], fourcc, 30,
-#			(frame.shape[1], frame.shape[0]), True)
-#        
-#        # some information on processing single frame
-#        if total > 0:
-#            elap = (end - start)
-#            print("[INFO] single frame took {:.4f} seconds".format(elap))
-#            print("[INFO] estimated total time to finish: {:.4f}".format(
-#				elap * total))
-#    print(frame_number)
-#    frame_number=frame_number+1
-#
-#	# write the output frame to disk
-#    writer.write(frame)
+    if writer is None:
+        # initialize our video writer
+        fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+        writer = cv2.VideoWriter(args["output"], fourcc, 30,
+			(frame.shape[1], frame.shape[0]), True)
+        
+        # some information on processing single frame
+        if total > 0:
+            elap = (end - start)
+            print("[INFO] single frame took {:.4f} seconds".format(elap))
+            print("[INFO] estimated total time to finish: {:.4f}".format(
+				elap * total))
+    print(frame_number)
+    frame_number=frame_number+1
+
+	# write the output frame to disk
+    writer.write(frame)
 
 # release the file pointers
 print("[INFO] cleaning up...")
